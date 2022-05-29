a26b81f728d711fc6b144b2b6fb4a88b1bde9a58
AntonKantardzhiev <kantarjiev88@abv.bg>
2022-05-29 18:03:14 +0200

    first commit

---
+++ includes/class-breex-site-manager-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://github.com/Salesmakers
+ * @since      1.0.0
+ *
+ * @package    Breex_Site_Manager
+ * @subpackage Breex_Site_Manager/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Breex_Site_Manager
+ * @subpackage Breex_Site_Manager/includes
+ * @author     Jeroen Knockaert <jeroen.knockaert@salesmakers.be>
+ */
+class Breex_Site_Manager_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-breex-site-manager-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://github.com/Salesmakers
+ * @since      1.0.0
+ *
+ * @package    Breex_Site_Manager
+ * @subpackage Breex_Site_Manager/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Breex_Site_Manager
+ * @subpackage Breex_Site_Manager/includes
+ * @author     Jeroen Knockaert <jeroen.knockaert@salesmakers.be>
+ */
+class Breex_Site_Manager_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
